dd2c3a687a978dfa739e4640cecdf70175a12b3d
S Alam <sacreative10@gmail.com>
2021-08-31 14:57:39 +0100

    Added PrettierRC

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,36 +1,36 @@
 module.exports = {
-  pathPrefix: "/reponame",
-  siteMetadata: {
-    siteUrl: `https://www.yourdomain.tld`,
-  },
-  plugins: [
-    "gatsby-plugin-sass",
-    "gatsby-plugin-image",
-    "gatsby-transformer-remark",
-    "gatsby-plugin-mdx",
-    "gatsby-plugin-sharp",
-    "gatsby-transformer-sharp",
-    {
-      resolve: "gatsby-source-filesystem",
-      options: {
-        name: "images",
-        path: "./src/images/",
-      },
-      __key: "images",
+    pathPrefix: '/reponame',
+    siteMetadata: {
+        siteUrl: `https://www.yourdomain.tld`,
     },
-    {
-      resolve: "gatsby-source-filesystem",
-      options: {
-        name: "pages",
-        path: "./src/pages/",
-      },
-      __key: "pages",
-       resolve: `gatsby-plugin-typescript`,
-      options: {
-        isTSX: true, // defaults to false
-        jsxPragma: `jsx`, // defaults to "React"
-        allExtensions: true, // defaults to false
-      },
-    },
-  ],
-};
+    plugins: [
+        'gatsby-plugin-sass',
+        'gatsby-plugin-image',
+        'gatsby-transformer-remark',
+        'gatsby-plugin-mdx',
+        'gatsby-plugin-sharp',
+        'gatsby-transformer-sharp',
+        {
+            resolve: 'gatsby-source-filesystem',
+            options: {
+                name: 'images',
+                path: './src/images/',
+            },
+            __key: 'images',
+        },
+        {
+            resolve: 'gatsby-source-filesystem',
+            options: {
+                name: 'pages',
+                path: './src/pages/',
+            },
+            __key: 'pages',
+            resolve: `gatsby-plugin-typescript`,
+            options: {
+                isTSX: true, // defaults to false
+                jsxPragma: `jsx`, // defaults to "React"
+                allExtensions: true, // defaults to false
+            },
+        },
+    ],
+}
--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,11 +1,12 @@
-import * as React from "react"
-
+import * as React from 'react'
 
 // markup
 const IndexPage = () => {
-  return (
-    <h1>HELLO ZA WARUDO test test1 test2</h1>
-  )
+    return (
+        <>
+            <h1>HELLO ZA WARUDO test test1 test2</h1>
+        </>
+    )
 }
 
 export default IndexPage
