2d9e55030b2955d054fe9795f0eca4c4db97b610
S Alam <sacreative10@gmail.com>
2021-08-31 11:36:09 +0100

    Finally added Gatsby Website, Will add gatsby pages actions in a future commit

---
+++ bstb_website/gatsby-config.js
@@ -0,0 +1,29 @@
+module.exports = {
+  siteMetadata: {
+    siteUrl: `https://www.yourdomain.tld`,
+  },
+  plugins: [
+    "gatsby-plugin-sass",
+    "gatsby-plugin-image",
+    "gatsby-transformer-remark",
+    "gatsby-plugin-mdx",
+    "gatsby-plugin-sharp",
+    "gatsby-transformer-sharp",
+    {
+      resolve: "gatsby-source-filesystem",
+      options: {
+        name: "images",
+        path: "./src/images/",
+      },
+      __key: "images",
+    },
+    {
+      resolve: "gatsby-source-filesystem",
+      options: {
+        name: "pages",
+        path: "./src/pages/",
+      },
+      __key: "pages",
+    },
+  ],
+};
