e2f76a9b3c9754e839567b7a0006bbaa0b007f48
sacreative10 <sacreative10@users.noreply.github.com>
2021-12-15 14:53:16 +0000

    deployed via Gatsby Publish Action 🎩 for 9b6b48eb4535ffc57e7529800ab4ce0e208a3433

---
+++ component---src-templates-blog-post-js-c316edd3607a41a1b850.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[5],{"8sOI":function(e,t,n){"use strict";n.r(t),n.d(t,"default",(function(){return l}));var a=n("q1tI"),r=n.n(a);function l(e){var t=e.data.markdownRemark;return r.a.createElement("div",null,r.a.createElement("h1",null,t.frontmatter.title),r.a.createElement("small",null,t.frontmatter.date),r.a.createElement("div",{dangerouslySetInnerHTML:{__html:t.html}}))}}}]);
+//# sourceMappingURL=component---src-templates-blog-post-js-c316edd3607a41a1b850.js.map
